30d1642d1864ab680cdc274afe55ef6fb17a74b7
LonggUwU <85337180+LonggUwU@users.noreply.github.com>
2021-06-04 14:52:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,6 +14,6 @@ const CONFIG = {
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Okiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?id=100023636538142'
 }
